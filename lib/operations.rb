936e43506f3ea19e44233378a0af2c74104c4a63
Taylor Booth <taylorkbooth@gmail.com>
2020-09-10 16:37:21 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
   if speed < 40
-    "unsafe"
-  else speed != '40< && 60>'
+    true
+  elsif speed > 60
+    true
+  else
+    false
   end
 
 end
 
-def not_safe?(speed)
-  speed == "40< || 60>" ? "safe" : "false"
-end
+# def not_safe?(speed)
+#   speed == "40< || 60>" ? "safe" : "false"
+# end
 
